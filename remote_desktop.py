0ab529f07d289f5ae83105d1c9e2dcdc6e689973
vinrox10 <46941233+vinrox10@users.noreply.github.com>
2025-04-27 20:23:57 -0400

    Update remote_desktop.py

--- remote_desktop.py
+++ remote_desktop.py
@@ -2,7 +2,6 @@ import os, subprocess, numpy as np
 import gradio as gr
 from mss import mss
 
-# point all GUI calls to our virtual display
 os.environ["DISPLAY"] = ":99"
 
 def capture_screen():
@@ -11,12 +10,10 @@ def capture_screen():
         return np.array(sct.grab(monitor))   # grab the virtual desktop
 
 def click_center():
-    # simulate a mouse click at the center of 1024×768
     subprocess.run(["xdotool", "mousemove", "512", "384", "click", "1"], check=True)
     return "Clicked at center"
 
 with gr.Blocks() as demo:
-    # pass height directly instead of using .style()
     img = gr.Image(type="numpy", label="Virtual Desktop", height=480)  
     btn = gr.Button("Click Center")
     btn.click(click_center, None, None)
